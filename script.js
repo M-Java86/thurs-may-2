ee61d710db5c95ac26138a828846b9565895ad50
Michelle Owens <mpythondata@outlook.com>
2019-05-02 20:34:54 -0400

    third commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// --------Old Work ------------------
+var two=2
+var onefourth="1/4"
+var firstName = "Michelle "
+var lastname=" Owens"
+
+function marinarasauce(cookname){
+    cookname = "michelle";
+    console.log("Hey " +  cookname + "! this is my receipe")
+    console.log("heat a medium-large saucepan over medium heat");
+    console.log("add " + two + " tspns of oil and 5 carlic cloves, cook until golden,about 2 minuntes");
+    console.log("add "+ onefourth + "cup 2 cans of crushed tomatoes, and salt and season with black pepper to taste");
+
+}
+marinarasauce()
+console.log ("owens")
+console.log (firstName + lastname)
+
+
+// -----------New Work Below----------------
